5bccceaa3a7ae02286c8c9d0cee97d525f22700a
sathwikareddy <51473246+sathwikareddy@users.noreply.github.com>
2019-06-07 14:30:56 +0530

    Add files via upload

---
+++ LED2.ino
@@ -0,0 +1,16 @@
+int state;
+void setup() {
+
+  // put your setup code here, to run once:
+pinMode(D0,INPUT);
+pinMode(D2,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ state=digitalRead(D2);
+ Serial.println(state);
+ digitalWrite(D2,state);
+ delay(1000);
+}
