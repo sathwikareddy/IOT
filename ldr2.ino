80e64130062c3909e639c8bbf0ba264a082d6aca
sathwikareddy <51473246+sathwikareddy@users.noreply.github.com>
2019-06-07 14:27:58 +0530

    Add files via upload

---
+++ ldr2.ino
@@ -0,0 +1,21 @@
+
+int ldr;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  ldr=analogRead(A0);
+  Serial.print("LDR Value:");
+  Serial.println(ldr);
+  int mv=map(ldr,0,1024,0,255);
+  Serial.print("Mapped LDR Value:");
+  Serial.println(mv);
+  analogwrite(D2,mv);
+  delay(500);
+  
+
+}
