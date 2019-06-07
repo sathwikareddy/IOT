4ce84437a9e127cfa373811338620fb258d91c23
sathwikareddy <51473246+sathwikareddy@users.noreply.github.com>
2019-06-07 12:27:50 +0530

    Add files via upload

---
+++ LED1.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(D2,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(D2,HIGH);
+delay(10000);
+digitalWrite(D2,LOW);
+delay(10000);
+}
